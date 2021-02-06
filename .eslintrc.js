cf9dbcdf8f316f368e7d93bc9401e20022b194e5
imekachi <imekachi@gmail.com>
2021-02-06 15:59:02 +0700

    chore: update eslint to require arrow body as needed

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,12 @@ module.exports = {
   extends: ['react-app', 'plugin:prettier/recommended'],
   plugins: ['prettier'],
   rules: {
+    // Includes .prettierrc.js rules
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
     // This rule is not compatible with Next.js's <Link /> components
     'jsx-a11y/anchor-is-valid': 'off',
     // No unused variables
     '@typescript-eslint/no-unused-vars': 'error',
-    // Includes .prettierrc.js rules
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'arrow-body-style': ['error', 'as-needed'],
   },
 }
