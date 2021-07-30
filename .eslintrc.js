a67ffb833c99867cbababc0f277cf204a1e38898
imekachi <imekachi@gmail.com>
2021-07-30 16:03:44 +0700

    chore: upgrade dependencies
    - upgrade next@11 and add eslint integration
    - upgrade husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['react-app', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
+  extends: [
+    'react-app',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
-    // Includes .prettierrc.js rules
-    'prettier/prettier': 'error',
     // This rule is not compatible with Next.js's <Link /> components
     'jsx-a11y/anchor-is-valid': 'off',
     // No unused variables
